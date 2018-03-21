9082bba5c3ac097c8694af5416fba196c2b2e12a
robin9104 <tjfrl3140@naver.com>
2018-03-21 20:27:41 +0900

    Add temperature saving program

---
+++ web.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+var seq = 0
+app.get('/update', function(req, res) {
+	fs.appendFile('log.txt', JSON.stringify(req.query)+"\n", function (err) {
+		if( err ) throw err
+		console.log("%j",req.query)
+		res.send("Got "+ String(seq++) +" "+ JSON.stringify(req.query))
+	});	
+})
+
+app.get('/get', function(req,res) {
+	var arr2=""	
+	var arr = fs.readFileSync('log.txt').toString().split("\n")
+	for(var i = 0 ; i < parseInt(Object.values(req.query)) ; i++){
+		arr2 = arr2 + arr[i]+"<br>" 
+		console.log(arr[i])
+	}
+	res.send(arr2)	
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
